71b15e3ba9d333bf50771ad3ccc42a3622922311
Vinicius <viniciusgoncalves2000@hotmail.com>
2025-09-28 12:27:26 -0300

    added normalize and rotate around functions

--- src/vector-utils.ts
+++ src/vector-utils.ts
@@ -29,4 +29,38 @@ export namespace VectorUtils {
 	export function scaleAndAdd(a: [number, number, number], b: [number, number, number], scale: number): [number, number, number] {
     	return [a[0] + b[0] * scale, a[1] + b[1] * scale, a[2] + b[2] * scale];
 	}
+
+	export function normalize(vector: [number, number, number]): [number, number, number] {
+		const [x, y, z] = vector;
+    	const length = Math.hypot(x, y, z);
+    	const nx = vector[0] / length;
+    	const ny = vector[1] / length;
+    	const nz = vector[2] / length;
+		return [nx, ny, nz];
+	}
+
+	export function rotateAround(point: [number, number, number], axis: [number, number, number], angle: number): [number, number, number] {
+    	const [x, y, z] = point;
+    	const [u, v, w] = normalize(axis);
+
+    	const cosA = Math.cos(angle);
+    	const sinA = Math.sin(angle);
+
+    	const newX =
+    	    u * (u * x + v * y + w * z) * (1 - cosA) +
+    	    x * cosA +
+    	    (-w * y + v * z) * sinA;
+
+    	const newY =
+    	    v * (u * x + v * y + w * z) * (1 - cosA) +
+    	    y * cosA +
+    	    (w * x - u * z) * sinA;
+
+    	const newZ =
+    	    w * (u * x + v * y + w * z) * (1 - cosA) +
+    	    z * cosA +
+    	    (-v * x + u * y) * sinA;
+
+    	return [newX, newY, newZ];
+	}
 }
