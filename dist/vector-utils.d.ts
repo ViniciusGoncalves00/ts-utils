c1fff0405a6007d093453dce19a92e17a09f00b8
Vinicius <viniciusgoncalves2000@hotmail.com>
2025-09-28 12:32:37 -0300

    added normalize and rotate around functions

--- dist/vector-utils.d.ts
+++ dist/vector-utils.d.ts
@@ -1,7 +1,9 @@
 export declare namespace VectorUtils {
     function intersectRayPlane(origin: [number, number, number], direction: [number, number, number], planeNormal: [number, number, number], planeOrigin: [number, number, number]): number | null;
-    function isParallel(dot: number, epsilon?: number): boolean;
+    function isParallel(dot: number, tolerance?: number): boolean;
     function dot(a: [number, number, number], b: [number, number, number]): number;
     function subtract(a: [number, number, number], b: [number, number, number]): [number, number, number];
     function scaleAndAdd(a: [number, number, number], b: [number, number, number], scale: number): [number, number, number];
+    function normalize(vector: [number, number, number]): [number, number, number];
+    function rotateAround(point: [number, number, number], axis: [number, number, number], angle: number): [number, number, number];
 }
--- dist/vector-utils.js
+++ dist/vector-utils.js
@@ -9,8 +9,8 @@ export var VectorUtils;
         return t >= 0 ? t : null;
     }
     VectorUtils.intersectRayPlane = intersectRayPlane;
-    function isParallel(dot, epsilon = 1e-6) {
-        return Math.abs(dot) < epsilon ? true : false;
+    function isParallel(dot, tolerance = 1e-6) {
+        return Math.abs(dot) < tolerance ? true : false;
     }
     VectorUtils.isParallel = isParallel;
     function dot(a, b) {
@@ -25,4 +25,30 @@ export var VectorUtils;
         return [a[0] + b[0] * scale, a[1] + b[1] * scale, a[2] + b[2] * scale];
     }
     VectorUtils.scaleAndAdd = scaleAndAdd;
+    function normalize(vector) {
+        const [x, y, z] = vector;
+        const length = Math.hypot(x, y, z);
+        const nx = vector[0] / length;
+        const ny = vector[1] / length;
+        const nz = vector[2] / length;
+        return [nx, ny, nz];
+    }
+    VectorUtils.normalize = normalize;
+    function rotateAround(point, axis, angle) {
+        const [x, y, z] = point;
+        const [u, v, w] = normalize(axis);
+        const cosA = Math.cos(angle);
+        const sinA = Math.sin(angle);
+        const newX = u * (u * x + v * y + w * z) * (1 - cosA) +
+            x * cosA +
+            (-w * y + v * z) * sinA;
+        const newY = v * (u * x + v * y + w * z) * (1 - cosA) +
+            y * cosA +
+            (w * x - u * z) * sinA;
+        const newZ = w * (u * x + v * y + w * z) * (1 - cosA) +
+            z * cosA +
+            (-v * x + u * y) * sinA;
+        return [newX, newY, newZ];
+    }
+    VectorUtils.rotateAround = rotateAround;
 })(VectorUtils || (VectorUtils = {}));
